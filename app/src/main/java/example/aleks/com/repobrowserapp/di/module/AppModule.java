a065ce9b50328dd02d09fb458f1eac5aab8e43b0
Aleksandar Aleksiev <aleksandar.aleksiev@styloko.com>
2018-05-06 11:11:57 +0100

    viewmodel factory

--- app/src/main/java/example/aleks/com/repobrowserapp/di/module/AppModule.java
+++ app/src/main/java/example/aleks/com/repobrowserapp/di/module/AppModule.java
@@ -1,11 +1,14 @@
 package example.aleks.com.repobrowserapp.di.module;
 
+import android.arch.lifecycle.ViewModelProvider;
+
 import dagger.Binds;
 import dagger.Module;
 import example.aleks.com.repobrowserapp.domain.interactor.authenticate.AuthenticateInteractor;
 import example.aleks.com.repobrowserapp.domain.interactor.authenticate.IAuthenticateInteractor;
 import example.aleks.com.repobrowserapp.domain.repository.local.storage.ILocalStorageRepository;
 import example.aleks.com.repobrowserapp.domain.repository.local.storage.LocalStorageRepository;
+import example.aleks.com.repobrowserapp.presentation.model.ViewModelFactory;
 
 /**
  * Created by aleks on 05/05/2018.
@@ -19,4 +22,7 @@ public abstract class AppModule {
 
     @Binds
     public abstract ILocalStorageRepository bindsLocalStorageRepository(LocalStorageRepository localStorageRepository);
+
+    @Binds
+    public abstract ViewModelProvider.Factory bindsViewModelProviderFactory(ViewModelFactory viewModelFactory);
 }
